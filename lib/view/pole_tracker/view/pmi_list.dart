714fd69d48175f2fdd52204ff2f435a6e17b2ee7
Bhavana <tiruveedulabhavana96@gmailcom>
2025-06-12 10:53:42 +0530

    Bhavana [12-06-2025]  PMI List

---
+++ lib/view/pole_tracker/view/pmi_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:tsnpdcl_employee/utils/general_routes.dart';
+
+class PmiList extends StatelessWidget {
+  const PmiList({super.key, required this.args});
+  static const id = Routes.pmiList;
+  final Map<String, dynamic> args;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
